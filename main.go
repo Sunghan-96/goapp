f7a052ee4413a6e6d629b92eef0a8d585c5f5876
Sunghan-96 <77769545+Sunghan-96@users.noreply.github.com>
2021-01-22 11:59:00 +0900

    Update main.go

--- main.go
+++ main.go
@@ -1,22 +1,22 @@
 package main
 
 import (
-"fmt"
-"os"
-"log"
-"net/http"
+  "fmt"
+  "os"
+  "log"
+  "net/http"
 )
 
 func handler(w http.ResponseWriter, r *http.Request){
-
-name, err := os.Hostname()
-if err != nil {
-panic(err)
-}
-fmt Fprintln(w,"hostname:",name)
+  
+  name, err := os.hostname()
+  if err != nil {
+    panic(err) 
+  }
+  fmt.Fprintln(w, "hostname:",name)
 }
 func main() {
-fmt.Fprintln(os.Stdout,"Starting Goapp Server......")
-http.HandleFunc("/",handler)
-log.Fatal(http.ListenAndServe(":8080",nil))
+  fmt.Fprintln(os.Stdout, "Starting GoApp Server......")
+  http.HandleFunc("/",handler)
+  log.Fatal(http.ListenAndServe(":8080",nil))
 }
